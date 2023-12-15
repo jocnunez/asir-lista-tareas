c40ed9e89bc1abc56a4011d6fc07ead953d499ae
jocnunez <jose.nunnez@gmail.com>
2023-12-15 20:59:27 +0100

    first commit

---
+++ web/main.js
@@ -0,0 +1,11 @@
+{
+    console.log("hola mundo")
+    let tarea = document.getElementById("tarea")
+    let boton = document.getElementById("botonAd")
+
+    boton.addEventListener("click", anadir)
+
+    function anadir() {
+        alert(tarea.value)
+    }
+}
